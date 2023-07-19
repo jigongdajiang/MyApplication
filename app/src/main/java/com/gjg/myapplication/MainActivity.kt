d4d1756cf4fd96b44bc8f66a3a67c779e86fbb3d
jigongdajiang <gaoji_gong@126.com>
2023-07-19 16:26:10 +0800

    test pre push 1

--- app/src/main/java/com/gjg/myapplication/MainActivity.kt
+++ app/src/main/java/com/gjg/myapplication/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! 4",
+        text = "Hello $name! 5",
         modifier = modifier
     )
 }
